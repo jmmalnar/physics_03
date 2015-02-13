89432684a6b17343d8b7ef23f22f293fe58ceb97
Jason Malnar <jmmalnar@utexas.edu>
2015-02-13 11:14:12 -0600

    I don't remember what I did here

--- config/routes.rb
+++ config/routes.rb
@@ -7,11 +7,13 @@ Rails.application.routes.draw do
 
   get 'main/main'
 
+  get 'webGL'
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
-  # root 'welcome#index'
+  root 'main#main'
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
